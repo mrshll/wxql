906ee70a4edddbe1ecbf4afdd8a380fd739ac880
mrshll <m@mrshll.com>
2023-12-26 19:55:42 -0500

    update grammar

--- wxql.ts
+++ wxql.ts
@@ -1,11 +1,11 @@
-import * as ohm from "https://cdn.skypack.dev/ohm-js";
+import * as ohm from 'https://cdn.skypack.dev/ohm-js';
 
-const queryGrammar = ohm.grammar(await Deno.readTextFile("./queryGrammar.ohm"));
+const queryGrammar = ohm.grammar(await Deno.readTextFile('./queryGrammar.ohm'));
 
-const userInput = "show precipitation and temperature";
+const userInput = 'show precipitation and temperature in the last 10 days';
 const m = queryGrammar.match(userInput);
 if (m.succeeded()) {
-  console.log("valid query");
+  console.log('valid query');
 } else {
-  console.log("invalid query");
+  console.log('invalid query');
 }
